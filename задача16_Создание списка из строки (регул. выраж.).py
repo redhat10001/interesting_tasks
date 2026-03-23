57cda7878f9c932a2d445d17e6cfc8129673d5c9
redhat10001 <redhat10001@gmail.com>
2026-03-23 18:41:05 +0300

    Строка в список

--- задача16_Создание списка из строки (регул. выраж.).py
+++ задача16_Создание списка из строки (регул. выраж.).py
@@ -2,7 +2,7 @@ print('*' * 25)
 
 import re
 
-s = 'Мама мыла раму'
+s = 'Последняя осень ни сторчи ни слова'
 
 RE_WORD = re.compile(r'\w+')
 rez = RE_WORD.findall(s) # возможно использование s.lower() и s.upper()
